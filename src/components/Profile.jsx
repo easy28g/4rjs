b32cdaa1b7631616791560ae8993ee17460ae1af
easy28g <fj@tk$q]3v>
2022-04-06 16:58:33 +0600

    rjs13l

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return(
+        <div className='content'>
+          <div>
+            <img src='https://v-thailand.com/wp-content/uploads/2015/01/Plyazhi-Samui.jpg' />
+          </div>
+          <div>
+            ava+description
+          </div>
+          <div>
+            My posts
+            <div>
+              New post
+            </div>
+          </div>
+          <div>
+            <div>
+              Post1
+            </div>
+            <div>
+              Post2
+            </div>
+          </div>
+        </div>
+    );
+}
+
+export default Profile;
